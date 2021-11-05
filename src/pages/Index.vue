74ba225bf0d656ac863195d88f4df303b7cc6dfb
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-11-05 16:40:00 +0800

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+	 <n-layout>
+      <n-layout-header>颐和园路</n-layout-header>
+      <n-layout-content content-style="padding: 24px;">平山道</n-layout-content>
+      <n-layout-footer>成府路</n-layout-footer>
+    </n-layout>
+</template>
+
+<script setup>
+import { NLayout, NLayoutHeader,NLayoutContent,NLayoutFooter } from "naive-ui"
+</script>
