21df35399f62b9d0c7b48a6976c85265bbefae4e
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2022-01-28 20:15:40 +0800

    and

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
@@ -8,5 +10,6 @@ app.config.globalProperties.$getpic = (name) => {
 	return new URL(`../src/assets/${name}`, import.meta.url).href;
 }
 
+app.use(Antd);
 app.use(router)
 app.mount('#app')
